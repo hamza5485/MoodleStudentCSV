64a889f7867b934125e00f136cf92d954e20b597
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-04-08 09:50:27 +0500

    added cohort

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ courses = [
     'ITIC-DS',  # Data Science
     'CCIRM'  # Cybersecurity
 ]
+COHORT = 'STUDENT_FEB_2021'  # change me for a new cohort -- update me for dynamic cohort scheme
 for row in csv_file:
     course = ''
     if row[5] == DATA_SCIENCE:
         course = courses[0]
     elif row[5] == CYBER_SEC or row[5] == CYBER_SEC_NETWORKING:
         course = courses[1]
-    #                    username, fname,        lname,          email,  course, role
-    student_data.append([row[4], row[1].title(), row[2].title(), row[4], course, 'student'])
+    #                    username, fname,        lname,          email,  cohort, course, role
+    student_data.append([row[4], row[1].title(), row[2].title(), row[4], COHORT, course, 'student'])
 
 with open('students.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['username', 'firstname', 'lastname', 'email', 'course1', 'role1'])
+    writer.writerow(['username', 'firstname', 'lastname', 'email', 'cohort1', 'course1', 'role1'])
     writer.writerows(student_data)
